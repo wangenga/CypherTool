de7d1f10674dd7cb68ffa7c5ea665ec6aca492a1
wangenga <daisyimg65@gmail.com>
2026-07-24 15:01:53 +0300

    Implement placeholder methods in CypherTool for input and encryption/decryption algorithms

--- CypherTool.java
+++ CypherTool.java
@@ -1,14 +1,24 @@
 public class CypherTool {
     public static void main(String[] args) {}
 
-    public static InputData getInput() {}
+    public static InputData getInput() {
+        return null;
+    }
 
-    public static String encryptRot13(String s) {}
+    public static String encryptRot13(String s) {
+        return "";
+    }
 
-    public static String encryptAtbash(String s) {}
+    public static String encryptAtbash(String s) {
+        return "";
+    }
 
-    public static String decryptRot13(String s) {}
+    public static String decryptRot13(String s) {
+        return "";
+    }
 
-    public static String decryptAtbash(String s) {}
+    public static String decryptAtbash(String s) {
+        return "";
+    }
 
 }
