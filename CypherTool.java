3b3d95a82851e01571f489bc419a526e77112256
benjaminmaigua <benjaminmaiguake@gmail.com>
2026-07-24 14:40:07 +0300

    cyphertoolfile

---
+++ CypherTool.java
@@ -0,0 +1,14 @@
+public class CypherTool {
+    public static void main(String[] args) {}
+
+    public static InputData getInput() {}
+
+    public static String encryptRot13(String s) {}
+
+    public static String encryptAtbash(String s) {}
+
+    public static String decryptRot13(String s) {}
+
+    public static String decryptAtbash(String s) {}
+
+}
