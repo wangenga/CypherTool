43be749d4f0c4537dc14821fac5d519f08c6fa67
wangenga <daisyimg65@gmail.com>
2026-07-24 16:07:09 +0300

    Enhance CypherTool with user input prompts for operation selection and message entry

--- CypherTool.java
+++ CypherTool.java
@@ -8,7 +8,30 @@ public class CypherTool {
     }
 
     public static InputData getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select operation:");
+        System.out.println("1. Encrypt");
+        System.out.println("2. Decrypt");
+        System.out.println("$> ");
+
+        int ops = scanner.nextInt();
+
+        System.out.println("Select cypher: ");
+        System.out.println("1. ROT13");
+        System.out.println("2. Atbash");
+        System.out.println("$> ");
+
+        int cyp = scanner.nextInt();
+
+        Scanner.nextLine();
+
+        System.out.println("/nEnter message: ");
+        System.out.println("$> ");
+
+        Scanner.nextLine();
+
         return null;
+
     }
 
     public static String encryptRot13(String s) {
